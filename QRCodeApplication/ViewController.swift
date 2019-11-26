022bff6a5b4d0b9ce8d4f2f27255b72a5275b68d
Victor Jong <s10186045@connect.np.edu.sg>
2019-11-26 17:40:51 +0800

    Initial Commit

---
+++ QRCodeApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCodeApplication
+//
+//  Created by Victor Jong on 26/11/19.
+//  Copyright © 2019 Victor Jong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
